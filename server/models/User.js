5920ff4e78485a061ecf7978bda02189dd5801e7
Aaditya-2407 <kharataaditya2407@gmail.com>
2026-09-03 23:03:44 +0530

    added userSchema for user pre save password hook and password compare method using bcrypt

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+const { Schema } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username:{
+            type: String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true,
+        },
+        email:{
+            type:String,
+            required: true,
+            trim:true,
+            unique:true,
+            lowercase:true,
+        },
+        password:{
+            type:String,
+            required:[true, "Password is required"],
+        },
+        refreshToken: {
+type: String,
+},
+forgotPasswordToken: { type: String },
+forgotPasswordExpiry: { type: Date },
+emailVerificationToken: { type: String },
+emailVerificationExpiry: { type: Date },
+},
+{ timestamps: true }
+    
+)
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10);
+})
+userSchema.methods.comparePassword = async function(password)
+{
+    return await bcrypt.compare(password, this.password);
+};
